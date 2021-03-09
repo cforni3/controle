73ab33dcae8fc99c0d59d00e8f0bffb581765204
Carla Frn <carla.forni3011@gmail.com>
2021-03-09 09:53:10 +0000

    vider

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-def puiss(c,liste):
-  a= [x**c for x  in liste]
-  return a 
 
-d = puiss(3,[4,5])
-print (d) 
 
-def puiss_etoile(nb, b ,**options):
-  li= [x**nb for x  in options]
-  return li 
 
-assert puiss_etoile(2,4,5) == [16, 25]
